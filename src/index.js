c41b3d7b665b4e45e3eaa95b5ad43953f7ef99f2
bcharlotin1 <biancacharlotin1@gmail.com>
2021-09-04 10:18:01 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  const form = document.getElementById("create-task-form");
+  const addToList = document.getElementById("tasks");
+
+  form.addEventListener("submit", handleSubmit);
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    let item = document.getElementById("new-task-description").value
+
+    addToList.innerHTML += `
+    <li class= "item">${item} </li>
+    <button class ="item-button"> Delete </button>
+
+    `
+   const buttons = document.getElementsByClassName("item-button")
+
+  for(let i = 0; i < buttons.length; i++){
+    buttons[i].addEventListener("click", deleteButton)
+  }
+
+  function deleteButton(event) {
+    event.target.parentElement.remove()
+    
+  }
+  }
+
+  
+  
+
 });
